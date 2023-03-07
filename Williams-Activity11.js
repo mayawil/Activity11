f9dc98a8a6204b685bf8ade5d0c3f9ec1619c5d6
mayawil <mayabethwilliams@gmail.com>
2023-03-07 13:26:28 -0500

    first commit

---
+++ Williams-Activity11.js
@@ -0,0 +1,11 @@
+//!-- jQuery call to the accordion() method.
+$(document).ready(function() {
+    $("#accordion").accordion({
+        // can click on each accordian
+        event: "click",
+        // will be as tall as its content
+        heightStyle: "content",
+        // all can be collapsed
+        collapsible: "true",
+    });
+});
